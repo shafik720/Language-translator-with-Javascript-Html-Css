250aa16d7d9f2354ae545253f4f92ca5279df2b5
shafik720 <shafikrasel5@gmail.com>
2022-10-11 13:55:48 +0600

    dsfs

--- app.js
+++ app.js
@@ -2,9 +2,14 @@
 const selectMenu = document.querySelectorAll('.icon-section select');
 
 for(let i=0; i<selectMenu.length;i++){
+    let selected;
     for(let countryCode in countries){
-        let option = ` <option value="">${countries[countryCode]}</option>`
-
+        if(i==0){
+            selected = countryCode == 'en-GB' ? 'selected' : '';
+        }else if(i==1){
+            selected = countryCode == 'bn-IN' ? 'selected' : '';
+        }
+        let option = ` <option value="${countryCode}" ${selected}>${countries[countryCode]}</option>`;
         selectMenu[i].insertAdjacentHTML('beforeend',option);
     }
 }
