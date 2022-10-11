23514a12c92b7e1aff79e3299cf8e962a4e5bbea
shafik720 <shafikrasel5@gmail.com>
2022-10-11 14:22:37 +0600

    dfgdsv

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 
-const selectMenu = document.querySelectorAll('.icon-section select');
+const selectMenu = document.querySelectorAll('.icon-section select'),
+translateBtn = document.querySelector('.container button'),
+from = document.querySelector('.left-section textarea'),
+to = document.querySelector('.right-section textarea'),
+fromValue = document.querySelector('.left-icon-section select'),
+toValue = document.querySelector('.right-icon-section select')
+;
+
+
+let url ;
 
 for(let i=0; i<selectMenu.length;i++){
     let selected;
@@ -13,3 +22,13 @@ for(let i=0; i<selectMenu.length;i++){
         selectMenu[i].insertAdjacentHTML('beforeend',option);
     }
 }
+
+translateBtn.addEventListener('click',()=>{
+    // console.log(fromValue.value, toValue.value)
+    url =  `https://api.mymemory.translated.net/get?q=${from.value}!&langpair=${fromValue.value}|${toValue.value}`;
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>{
+        to.value = data.responseData.translatedText;
+    });
+})
